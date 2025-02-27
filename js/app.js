b990561621ca55f2bd6a1ede6ff16b106dc0c683
Fridavhaf <frhaf7974@oslomet.no>
2025-02-27 20:31:15 +0100

    fixed bug with "fade in"

--- js/app.js
+++ js/app.js
@@ -9,20 +9,21 @@ hamMenu.addEventListener('click', () => {
 
 window.addEventListener('scroll', reveal);
 
-function reveal(){
-    var reveals = document.querySelectorAll('.reveal');
+function reveal() {
+    // Sjekk om hamburgermenyen er aktiv
+    if (!hamMenu.classList.contains('active')) {
+        var reveals = document.querySelectorAll('.reveal');
 
-    for(var i = 0; i < reveals.length; i++){
+        for (var i = 0; i < reveals.length; i++) {
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 150;
 
-        var windowheight = window.innerHeight;
-        var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
-
-        if(revealtop < windowheight - revealpoint){
-            reveals[i].classList.add('active');
-        }
-        else{
-            reveals[i].classList.remove('active');
+            if (revealtop < windowheight - revealpoint) {
+                reveals[i].classList.add('active');
+            } else {
+                reveals[i].classList.remove('active');
+            }
         }
     }
 }
